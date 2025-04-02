35d98f15bb133d99da6c43d0c6b424b61ba9bb4e
LucasBaliu <lucasbaliu@gmail.com>
2025-04-02 10:42:09 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -12,34 +12,33 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
+
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
 
 	@ManyToMany
-	@JoinTable(name = "tb_product_category",
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
-	public Product() {
-		
-	}
-	
 
-	public Product(Integer id, String name, String description, Double price, String imgUrl) {
+	public Product() {
+
+	}
+
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -48,67 +47,54 @@ public class Product implements Serializable{
 		this.imgUrl = imgUrl;
 	}
 
-
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public String getDescription() {
 		return description;
 	}
 
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 
 	public Double getPrice() {
 		return price;
 	}
 
-
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
 
 	public String getImgUrl() {
 		return imgUrl;
 	}
 
-
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-
 
 	public Set<Category> getCategories() {
 		return categories;
 	}
 
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPk.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPk.java
@@ -22,7 +22,7 @@ public class OrderItemPk implements Serializable{
 	@JoinColumn(name = "product_id")
 	private Product product;
 	
-	
+
 	public Order getOrder() {
 		return order;
 	}
